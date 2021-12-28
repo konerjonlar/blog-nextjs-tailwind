395398af82e9ddb577a83563d9257ac26539940a
Orhan <65315135+konerjonlar@users.noreply.github.com>
2021-12-28 05:28:01 +0300

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,5 +1,5 @@
 const siteMetadata = {
-  title: 'Sukha',
+  title: 'Hoş geldiniz',
   author: 'Orhan Cansu',
   headerTitle: 'Zaman bir illüzyon...',
   description: 'Yazacağım teknik bloglar Feynman`ın gösterdiği yolu izleyecekler.',
